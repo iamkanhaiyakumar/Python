1fc4a2d31a95d27b058f4e33b49fa88b9eb06718
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-07-03 13:08:08 +0530

    feat: Add code for variables and data types in chapter 2

---
+++ chapter 2/01-variables.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+c = 7
+
+print(a + b + c)
+
+name = "Kanhaiya"
+print(name)
---
+++ chapter 2/02-data types.py
@@ -0,0 +1,32 @@
+a = 1 # a is integer
+
+b = 5.33 # b is float point number
+
+c = "Hello" # c is string
+
+d = False # d is a boolen variable
+
+e = None # e is none type varible
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+# Output:
+# 1
+# 5.33
+# Hello
+# False
+# None
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
+# <class 'NoneType'>
+# Note: In Python, we do not need to declare the data type of a variable. It is automatically identified by the value assigned to it.
