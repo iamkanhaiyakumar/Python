fb8c2d12f56b3e8a64e9285ac3ad8148bb84754c
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-15 11:04:34 +0530

    Refactor code to prompt user for input and calculate sum of two numbers

--- practice set 2/problem1.py
+++ practice set 2/problem1.py
@@ -1,14 +1,18 @@
 #Write a python program to add two numbers
 
-a = 5
+# a = 5
 
-b = 7
+# b = 7
 
-print (a+b)
+# print (a+b)
 
-a = int("Enter a number: ")
-b = int("Enter number 2: ")
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
 
 print("Number a is: ", a)
 print("Number a is: ", b)
+
+
 print("sum of a and b is: ", a+b)
+
+
