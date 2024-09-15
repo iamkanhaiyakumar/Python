62b95250e9a895415e7070c5b2f11da5d66ce822
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-15 10:55:27 +0530

    Add multiple if statement for age eligibility check

---
+++ chapter 6/multiple_if_statement.py
@@ -0,0 +1,25 @@
+a = int(input("Enter your age: "))
+
+#if statement no:1
+if(a%2 == 0):
+    print("This is Even")
+#End of the 1 if statement
+
+
+#If statement no :2
+
+if(a>=18):
+    print("You are eligible to going to collage ")
+    print("Good luck for your future")
+
+elif(a<0):
+    print("Invalid age")
+
+else:
+    print("You are not eligible to going to collage ")
+    print("Try again next year")
+
+#End of the 2 if statement
+
+
+print("Wish you best")
