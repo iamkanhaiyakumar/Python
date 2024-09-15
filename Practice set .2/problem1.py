35f3d900b778409b80479e913670ccc1165bfed7
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-09-15 11:07:34 +0530

    Add code to prompt user for two numbers and calculate their sum

---
+++ Practice set .2/problem1.py
@@ -0,0 +1,18 @@
+#Write a python program to add two numbers
+
+# a = 5
+
+# b = 7
+
+# print (a+b)
+
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+
+print("Number a is: ", a)
+print("Number a is: ", b)
+
+
+print("sum of a and b is: ", a+b)
+
+
