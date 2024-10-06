cdfe4b15be474f3a6a77ed413927f7a0eb0e5a95
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-10-07 00:54:16 +0530

    Refactor code to use loops for printing numbers and improve game logic

---
+++ Project 1/short.py
@@ -0,0 +1,53 @@
+
+# comprehension of the code 
+
+
+import random
+
+'''
+1 for snake
+-1 for water
+0 for gun
+
+'''
+computer = random.choice([1, -1, 0])
+youstr = input("Enter your choice: ")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict = {1: "snake", -1: "water", 0: "gun"}
+
+you = youDict[youstr]
+
+print(f"You chose: {reverseDict[you]}\nComputer chose: {reverseDict[computer]}")
+
+if(computer == you):
+    print("It's a tie")
+
+else:
+
+#     if(computer == -1 and you == 1):
+#         print("You won")
+
+#     elif(computer == -1 and you == 0):
+#         print("You lose!")
+    
+#     elif(computer == 1 and you == -1):
+#         print("You lose!")
+    
+#     elif(computer == 0 and you == -1):
+#         print("You won")
+    
+#     elif(computer == 1 and you == 0):
+#         print("You won")
+    
+#     elif(computer == 0 and you == 1):
+#         print("You lose!")
+
+#     else:
+#         print("something went wrong") 
+
+######   comprehension of the code ######
+
+    if(computer - you == 1 or computer - you == -2):
+        print("You won")
+    else:
+        print("You lose!")
