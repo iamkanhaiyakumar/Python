1cafdbf181f4af372bafa35c9bffd7d67fee1a87
Kanhaiya Kumar <kanhaiyak0104@gmail.com.com>
2024-10-14 08:52:58 +0530

    Refactor code to use loops for printing numbers and improve game logic

---
+++ chapter 9/02_write.py
@@ -0,0 +1,7 @@
+str=" Hii Everyone, I am Kanhaiya Kumar.\n I am a student of B.Tech in Computer Science and Engineering.\n I am a student of LNCTE.\n I am a student of 3rd year.\n I am a student of 2022-2026 batch "
+
+f = open("myfile.txt", "w")
+
+f.write(str)
+
+f.close()
